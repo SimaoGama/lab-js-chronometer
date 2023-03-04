9b5e83f5063d13e7ece20bae5f23bd41b2e32a0f
Simão Gama <simaogama@MacBook-Pro-2.local>
2023-03-04 16:20:05 +0000

    Solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    let minutesPassed = this.currentTime / 60;
+    return Math.floor(minutesPassed);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    }
+    let secondsPassed = this.currentTime % 60;
+    return Math.floor(secondsPassed);
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0` + value;
+    }
+    return value.toString();
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
+
+//1 centisecond = 10 milliseconds
+// 1 second = 100 centiseconds = 1000 milliseconds
